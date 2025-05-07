b33d61d5b0330af0f99bbaeb1d80a98817c2805d
Henrique Velloso <henriquevelloso@gmail.com>
2025-05-06 22:34:04 -0300

    Removes RevenueCat dependency from Package.swift
    Removes the RevenueCat dependency from the Package.swift file.
    
    This change is made to avoid issues related to duplicate symbols during development. The RevenueCat dependency is now managed through a different mechanism, bypassing the Swift Package Manager.

--- Package.swift
+++ Package.swift
@@ -15,10 +15,10 @@ let package = Package(
             name: "PaywallKit",
             targets: ["PaywallKit"]),
     ],
-    dependencies: [
-        // RevenueCat via SPM
-        .package(url: "https://github.com/RevenueCat/purchases-ios.git", from: "4.0.0")
-    ],
+//    dependencies: [
+//        // RevenueCat via SPM
+//        .package(url: "https://github.com/RevenueCat/purchases-ios.git", from: "4.0.0")
+//    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite
         // Targets can depend on other targets in this package and products from dependencies
